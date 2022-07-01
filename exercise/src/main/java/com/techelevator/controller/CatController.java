2b5317c8eb3bf0cb4bde607c1466ea97a75a696b
Michael Felger <michaelfelger@gmail.com>
2022-07-01 15:30:00 -0400

    finished

--- exercise/src/main/java/com/techelevator/controller/CatController.java
+++ exercise/src/main/java/com/techelevator/controller/CatController.java
@@ -2,20 +2,18 @@ package com.techelevator.controller;
 
 import com.techelevator.dao.CatCardDao;
 import com.techelevator.model.CatCard;
-import com.techelevator.model.CatPic;
+import com.techelevator.model.CatCardNotFoundException;
 import com.techelevator.services.CatFactService;
 import com.techelevator.services.CatPicService;
-import com.techelevator.services.RestCatPicService;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/cards/")
+@RequestMapping("/api/cards")
 
 public class CatController {
 
@@ -30,12 +28,20 @@ public class CatController {
         this.catPicService = catPicService;
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @RequestMapping(path = "", method = RequestMethod.GET)
     public List<CatCard> list(){
-        catcards.get
+        return catCardDao.list();
     }
 
-    @RequestMapping(path = "random", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
+    public CatCard get(@PathVariable long id) {
+        return catCardDao.get(id);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(path = "/random", method = RequestMethod.GET)
     public CatCard getPic() {
         CatCard newRandomCard = new CatCard();
         newRandomCard.setImgUrl(catPicService.getPic().getFile());
@@ -43,6 +49,24 @@ public class CatController {
         return newRandomCard;
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
+    @RequestMapping(path = "", method = RequestMethod.POST)
+    public boolean save(@RequestBody CatCard catCard) {
+        return catCardDao.save(catCard);
+    }
+
+
+
+    @RequestMapping(path = "/{id}", method = RequestMethod.PUT)
+    public boolean update(@RequestBody @Valid CatCard catCard, @PathVariable long id) throws CatCardNotFoundException {
+        return catCardDao.update(id, catCard);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
+    public boolean delete(@PathVariable long id) throws CatCardNotFoundException {
+        return catCardDao.delete(id);
+    }
 
 }
 
