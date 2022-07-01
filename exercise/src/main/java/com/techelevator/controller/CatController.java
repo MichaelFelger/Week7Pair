b149ab5bd82c9b08eaf2d981b5be90aeaee36e63
Dylan Meek <meekdm@gmail.com>
2022-07-01 14:54:03 -0400

    V1

--- exercise/src/main/java/com/techelevator/controller/CatController.java
+++ exercise/src/main/java/com/techelevator/controller/CatController.java
@@ -1,14 +1,28 @@
 package com.techelevator.controller;
 
 import com.techelevator.dao.CatCardDao;
+import com.techelevator.model.CatCard;
+import com.techelevator.model.CatPic;
 import com.techelevator.services.CatFactService;
 import com.techelevator.services.CatPicService;
+import com.techelevator.services.RestCatPicService;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cards/")
 
 public class CatController {
 
     private CatCardDao catCardDao;
     private CatFactService catFactService;
     private CatPicService catPicService;
+    private List<CatCard> catcards = new ArrayList<>();
 
     public CatController(CatCardDao catCardDao, CatFactService catFactService, CatPicService catPicService) {
         this.catCardDao = catCardDao;
@@ -16,5 +30,19 @@ public class CatController {
         this.catPicService = catPicService;
     }
 
+    @RequestMapping(path = "", method = RequestMethod.GET)
+    public List<CatCard> list(){
+        catcards.get
+    }
+
+    @RequestMapping(path = "random", method = RequestMethod.GET)
+    public CatCard getPic() {
+        CatCard newRandomCard = new CatCard();
+        newRandomCard.setImgUrl(catPicService.getPic().getFile());
+        newRandomCard.setCatFact(catFactService.getFact().getText());
+        return newRandomCard;
+    }
+
 
 }
+
--- exercise/src/main/java/com/techelevator/services/RestCatPicService.java
+++ exercise/src/main/java/com/techelevator/services/RestCatPicService.java
@@ -1,16 +1,21 @@
 package com.techelevator.services;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import com.techelevator.model.CatPic;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.http.HttpRequest;
 
 @Component
 public class RestCatPicService implements CatPicService {
 
+	private RestTemplate restTemplate = new RestTemplate();
+
 	@Override
-	public CatPic getPic() {
-		// TODO Auto-generated method stub
-		return null;
+	public CatPic getPic(){
+	return restTemplate.getForObject("https://cat-data.netlify.app/api/pictures/random", CatPic.class);
 	}
 
 }	
